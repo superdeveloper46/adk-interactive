9d0d4e5ef1eefb88d2113703272fcda3bdde2b97
superdeveloper46 <topwebdev46@gmail.com>
2025-03-03 23:51:09 +0300

    integrated supabase for home section and add contact us functionality

--- src/components/pages/home/HomeSection.tsx
+++ src/components/pages/home/HomeSection.tsx
@@ -1,24 +1,53 @@
 "use client"
 
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { supabase } from "@/lib/supabaseClient";
 
 export default function HomeSection() {
+    const [content, setContent] = useState("");
+    const [imageUrl, setImageUrl] = useState("/home.png");
+    const router = useRouter();
+
+    useEffect(() => {
+        async function fetchMessage() {
+            const { data, error } = await supabase
+                .from("home")
+                .select("content, image")
+                .limit(1)
+                .single();
+            
+            console.log('data: ', data);
+            if (error) {
+                console.error("Error fetching message:", error);
+            } else {
+                setContent(data?.content || "No message found");
+                setImageUrl(data?.image || "No image found");
+            }
+        }
+
+        fetchMessage();
+    }, []);
+
+    const handleContactClick = () => {
+        router.push("/contact");
+    };
+
     return (
         <section className="section home">
             <div className='section-content'>
-                <h1 className="title fade-up">
-                    ADK Interactive is Nick Penree’s small but <span>mighty</span> software development company in Upstate New York. 
-                </h1>
+                <h1 className="title fade-up" dangerouslySetInnerHTML={{ __html: content }}></h1>
                 <Image
-                    src='/home.png'
+                    src={`${imageUrl}`}
                     alt="Blog home"
                     width={695}
                     height={405}
                     className='home-img fade-up'
                 />
                 <div className='section-action-container fade-up'>
-                    <Button className='default-btn'>
+                    <Button className='default-btn' onClick={handleContactClick}>
                         <span>
                             Contact Us
                         </span>
